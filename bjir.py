db42eb6d2b9e4b0bb6e8acabdd1bca5e9586e7eb
damar <damar.pradiptojati@sby.spil>
2023-09-06 15:41:27 +0700

    bjir

--- bjir.py
+++ bjir.py
@@ -1,55 +1,9 @@
-### google-chrome --remote-debugging-port=9014 --user-data-dir="Documents"
+## google-chrome --remote-debugging-port=9014 --user-data-dir="Documents"
 
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# import pyautogui
-
-# # import os, subprocess
-
-# # subprocess.run(["google-chrome", "--remote-debugging-port=9014", '--user-data-dir="Documents"'],shell=True)
-# # print("chuwaksz")
-
-# # os.system('google-chrome --remote-debugging-port=9014 --user-data-dir="Documents"')
-
-# pyautogui.click(x=200,y=200)
-
-# chrome_options = Options()
-# chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9014")
-# driver = webdriver.Chrome(options=chrome_options)
-# driver.get("https://www.tiktok.com/signup/phone-or-email/phone")
-# #find month
-# driver.find_element("xpath","//*[contains(text(), 'Month')]").click()
-# pyautogui.typewrite("july", interval=0.3)
-# pyautogui.press('enter')
-# # find day
-# driver.find_element("xpath","//*[contains(text(), 'Day')]").click()
-# pyautogui.typewrite("20", interval=0.3)
-# pyautogui.press('enter')
-# # find year
-# driver.find_element("xpath","//*[contains(text(), 'Year')]").click()
-# pyautogui.typewrite("1991", interval=0.3)
-# pyautogui.press('enter')
-# #find phone
-phone = str(67074098099)
-phone = str(67074145354)
-# separator = 3
-# phone_pt1 = phone[:separator]
-# phone_pt2 = phone[separator:]
-# print(phone_pt1,phone_pt2)
-# # driver.find_element("xpath","//input[@placeholder()= 'Phone number']").click()
-# pyautogui.press('tab')
-# pyautogui.press('tab')
-# pyautogui.press('enter')
-# pyautogui.typewrite("timor", interval=0.3)
-# pyautogui.press('enter')
-# pyautogui.press('tab')
-# pyautogui.typewrite("74098099", interval=0.3)
-# #click button send code
-# pyautogui.press('tab')
-# pyautogui.press('tab')
-# pyautogui.press('enter')
-# # driver.find_element(By.XPATH, '//button[text()="Send code"]').click()
-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pyautogui
+import time.sleep
 
 def getkey(phone):
     from urllib import request, parse
@@ -61,3 +15,54 @@ def getkey(phone):
     jsoncontent = json.loads(content)
     msg = jsoncontent[0]['msg']
     return msg.split(" ")[1]
+
+pyautogui.click(x=200,y=200)
+
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9014")
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.tiktok.com/signup/phone-or-email/phone")
+#find month
+driver.find_element("xpath","//*[contains(text(), 'Month')]").click()
+pyautogui.typewrite("july", interval=0.3)
+pyautogui.press('enter')
+# find day
+driver.find_element("xpath","//*[contains(text(), 'Day')]").click()
+pyautogui.typewrite("20", interval=0.3)
+pyautogui.press('enter')
+# find year
+driver.find_element("xpath","//*[contains(text(), 'Year')]").click()
+pyautogui.typewrite("1991", interval=0.3)
+pyautogui.press('enter')
+#find phone
+phone = str(67074145354)
+separator = 3
+phone_pt1 = phone[:separator]
+phone_pt2 = phone[separator:]
+print(phone_pt1,phone_pt2)
+# driver.find_element("xpath","//input[@placeholder()= 'Phone number']").click()
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('enter')
+pyautogui.typewrite("timor", interval=0.3)
+pyautogui.press('enter')
+pyautogui.press('tab')
+pyautogui.typewrite("74098099", interval=0.3)
+#click button send code
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('enter')
+sleep(30)
+#click register
+pyautogui.keyDown('shift')
+pyautogui.press('tab')
+pyautogui.keyUp('shift')
+otp = getkey(phone)
+pyautogui.typewrite(otp, interval=0.3)
+#submit data
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('enter')
+# driver.find_element(By.XPATH, '//button[text()="Send code"]').click()
+
+
